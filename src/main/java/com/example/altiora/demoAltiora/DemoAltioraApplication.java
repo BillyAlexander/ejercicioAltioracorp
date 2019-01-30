f15dd7dd74ab2bd53cefdfe0e005f4ff547d3a0d
Alex Villa <alex22_w@hotmail.com>
2019-01-29 20:42:48 -0500

    configuracion app

---
+++ src/main/java/com/example/altiora/demoAltiora/DemoAltioraApplication.java
@@ -0,0 +1,14 @@
+package com.example.altiora.demoAltiora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAltioraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAltioraApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/altiora/demoAltiora/DemoAltioraApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.altiora.demoAltiora;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoAltioraApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
